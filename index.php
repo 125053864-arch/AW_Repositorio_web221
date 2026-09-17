d36ca97b653f72b7871a867ad8432c7f291e9420
125053864-arch <125053864@upq.edu.mx>
2026-09-17 15:34:15 -0600

    Se agrego index.php para prueba de XAMPP

---
+++ index.php
@@ -0,0 +1,31 @@
+<!-- <?php
+    echo "Hola Mundo!"
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+        h1{
+            color: #359c82;;
+        }
+        
+
+    </style>
+</head>
+<body>
+    <h1>Hola crayola</h1>
+
+
+    <h2>Hola crayola</h2>
+    <h3>Hola crayola</h3>
+    <h4>Hola crayola</h4>
+    <h5>Hola crayola</h5>
+    <h6>Hola crayola</h6>
+   
+</body>
+</html>
