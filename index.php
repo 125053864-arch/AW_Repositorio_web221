f6ef239179da4aec1dade9a12e484fb949dedf26
125053864-arch <125053864@upq.edu.mx>
2026-09-24 15:30:43 -0600

    uso de flex y grid en archivo .html

--- index.php
+++ index.php
@@ -3,29 +3,61 @@
 ?> -->
 
 <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
 
-        h1{
-            color: #359c82;;
-        }
-        
-
-    </style>
+    <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
-    <h1>Hola crayola</h1>
+
+    <div>
+        <h1>Hola crayola</h1>
+
+        <h2>Hola crayola</h2>
+        <h3>Hola crayola</h3>
+        <h4>Hola crayola</h4>
+        <h5>Hola crayola</h5>
+        <h6>Hola carbon</h6>
+
+        <p>Estoy aprendiendo desarrollo web con HTML, CSS y PHP.</p>
+
+        <h2>Mi sección</h2>
+        <p>Este es el contenido de mi sección.</p>
+    </div>
+
+    <div class="seccion-verde">
+        <h2>Título del párrafo</h2>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+        </p>
+    </div>
+
+    <div class="seccion-roja">
+        <h2>Título del párrafo</h2>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+        </p>
+    </div>
 
 
-    <h2>Hola crayola</h2>
-    <h3>Hola crayola</h3>
-    <h4>Hola crayola</h4>
-    <h5>Hola crayola</h5>
-    <h6>Hola crayola</h6>
-   
 </body>
 </html>
